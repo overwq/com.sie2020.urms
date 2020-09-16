a22a57ff413f5f1a5ebeb0d2e9e77acb4c53736d
overwq <805894205@qq.com>
2020-09-17 01:14:29 +0800

    33

--- src/main/resources/demo/src/router/index.js
+++ src/main/resources/demo/src/router/index.js
@@ -1,43 +1,59 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import { Message } from 'element-ui'
+// import { Message } from 'element-ui'
 Vue.use(VueRouter)
 
 const routes = [
   {
+    path: '/',
+    component: () => import('../page/HelloWorld.vue')
+  },
+  {
+
     path: '/container',
-    component: () =>
-      import('../components/Container.vue'),
-    children: [{
-      path: 'user',
-      component: () =>
-        import('../page/user/User.vue')
-    },
-    {
-      path: 'role',
-      component: () =>
-        import('../page/role/Role.vue')
-    },
-    {
-      path: 'menu',
-      component: () =>
-        import('../page/menu/Menu.vue')
-    },
-    {
-      path: 'home',
-      component: () =>
-        import('../page/Home.vue')
-    }
+    component: () => import('../components/Container.vue'),
+    children: [
+      {
+        path: 'user',
+        component: () => import('../page/user/User.vue')
+      },
+      {
+        path: 'role',
+        component: () => import('../page/role/Role.vue')
+      },
+      {
+        path: 'menu',
+        component: () => import('../page/menu/Menu.vue')
+      },
+      {
+        path: 'home',
+        component: () => import('../page/Home.vue')
+      }
     ]
   },
 
   {
     path: '/login',
     name: 'Login',
+<<<<<<< HEAD
     component: () =>
       import('../page/Login.vue')
+<<<<<<< Branch_wqh_test
+=======
+  },
+  {
+    path: '/',
+    component: () =>
+      import('../page/Login.vue')
+=======
+    component: () => import('../page/Login.vue')
+  },
+  {
+    path: '/',
+    component: () => import('../page/Login.vue')
+>>>>>>> zhenghe
+>>>>>>> local
   }
-
 ]
 
 const router = new VueRouter({
@@ -45,7 +61,12 @@ const router = new VueRouter({
 })
 
 router.beforeEach((to, from, next) => {
+<<<<<<< Branch_wqh_test
   const user = sessionStorage.getItem('user')
+=======
+<<<<<<< HEAD
+  const user = localStorage.getItem('user')
+>>>>>>> local
   if (to.path === '/login' || (user && user.length > 0)) {
     next()
   } else {
@@ -53,6 +74,17 @@ router.beforeEach((to, from, next) => {
     Message.warning('请先登陆')
     next('/login')
   }
+=======
+  next()
+  // const user = localStorage.getItem('user')
+  // if (to.path === '/login' || (user && user.length > 0)) {
+  //   next()
+  // } else {
+  //   Message.closeAll()
+  //   Message.warning('请先登陆')
+  //   next('/login')
+  // }
+>>>>>>> zhenghe
 })
 
 export default router
